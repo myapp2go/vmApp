3b5726645e58a48f886bdf8cd5ab709b4eb72868
myapp2go <tapaulchen@gmail.com>
2015-05-24 22:04:36 -0700

    write

--- vm/src/com/pc/vm/WriteMailActivity.java
+++ vm/src/com/pc/vm/WriteMailActivity.java
@@ -5,11 +5,15 @@ import android.speech.tts.TextToSpeech;
 public abstract class WriteMailActivity extends ReadMailActivity {
 
 	protected void doWriteMail() {
+		String myEmail = "tapaulchen@gmail.com";
+		String myPassword = "Tsnsn1559";
+		
 		switch (subCommand) {
 		case Constants.COMMAND_NONE :
     		subCommand = Constants.SUBCOMMAND_SUBJECFT;
-    		tts.speak(Constants.COMMAND_SUBJECT_GREETING, TextToSpeech.QUEUE_ADD, map);
-    		startRecognizer();
+			new WriteMailTask(WriteMailActivity.this).execute(myEmail, myPassword);
+//    		tts.speak(Constants.COMMAND_SUBJECT_GREETING, TextToSpeech.QUEUE_ADD, map);
+//    		startRecognizer();
 			break;
 		case Constants.SUBCOMMAND_SUBJECFT :
     		subCommand = Constants.SUBCOMMAND_BODY;
@@ -20,6 +24,10 @@ public abstract class WriteMailActivity extends ReadMailActivity {
     		subCommand = Constants.SUBCOMMAND_DONE;
     		tts.speak(Constants.COMMAND_DONE_GREETING, TextToSpeech.QUEUE_ADD, map);
     		startRecognizer();	
+			break;
+		case Constants.SUBCOMMAND_DONE :
+
+			new WriteMailTask(WriteMailActivity.this).execute(myEmail, myPassword);
 			break;
 		}
 	}
