e4240fdfe32e0b3f86b955b28e746a4ade7ab1e5
myapp2go <tapaulchen@gmail.com>
2015-06-13 09:14:37 -0700

    reset read

--- AppVoice/src/com/timebyte/appvoice/MainActivity.java
+++ AppVoice/src/com/timebyte/appvoice/MainActivity.java
@@ -2,17 +2,30 @@ package com.timebyte.appvoice;
 
 import java.util.Arrays;
 import java.util.List;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+/*
 import com.facebook.Request;
 import com.facebook.Response;
 import com.facebook.Session;
@@ -21,31 +34,83 @@ import com.facebook.UiLifecycleHelper;
 import com.facebook.model.GraphUser;
 import com.facebook.widget.LoginButton;
 import com.facebook.widget.LoginButton.UserInfoChangedCallback;
-
+*/
 public class MainActivity extends FragmentActivity {
 
-	private LoginButton loginBtn;
+//	private LoginButton loginBtn;
 	private Button postImageBtn;
 	private Button updateStatusBtn;
 
 	private TextView userName;
 
-	private UiLifecycleHelper uiHelper;
+//	private UiLifecycleHelper uiHelper;
 
 	private static final List<String> PERMISSIONS = Arrays.asList("publish_actions");
 
 	private static String message = "Sample status posted from android app";
 
+	private CallbackManager callbackManager;
+	private LoginButton loginButton;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		uiHelper = new UiLifecycleHelper(this, statusCallback);
-		uiHelper.onCreate(savedInstanceState);
-
 		setContentView(R.layout.activity_main);
+		
+		FacebookSdk.sdkInitialize(this.getApplicationContext());
+
+		callbackManager = CallbackManager.Factory.create();
+		/*
+	    LoginManager.getInstance().registerCallback(callbackManager,
+	            new FacebookCallback<LoginResult>() {
+	                @Override
+	                public void onSuccess(LoginResult loginResult) {
+	                    // App code
+	                	System.out.println("**** onSuccess");
+	                }
+
+	                @Override
+	                public void onCancel() {
+	                     // App code
+	                	System.out.println("**** onCancel");
+	                }
+
+	                @Override
+	                public void onError(FacebookException exception) {
+	                     // App code  
+	                	System.out.println("**** onError");
+	                }
+	    });
+*/	    
+		loginButton = (LoginButton) findViewById(R.id.login_button);
+		
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+			@Override
+			public void onSuccess(LoginResult result) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onCancel() {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onError(FacebookException error) {
+				// TODO Auto-generated method stub
+				
+			}  });
+
+//		uiHelper = new UiLifecycleHelper(this, statusCallback);
+//		uiHelper.onCreate(savedInstanceState);
+
+//		setContentView(R.layout.activity_main);
 
 		userName = (TextView) findViewById(R.id.user_name);
+/*
 		loginBtn = (LoginButton) findViewById(R.id.fb_login_button);
 		loginBtn.setUserInfoChangedCallback(new UserInfoChangedCallback() {
 			@Override
@@ -57,13 +122,13 @@ public class MainActivity extends FragmentActivity {
 				}
 			}
 		});
-
+*/
 		postImageBtn = (Button) findViewById(R.id.post_image);
 		postImageBtn.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View view) {
-				postImage();
+//				postImage();
 			}
 		});
 
@@ -76,9 +141,41 @@ public class MainActivity extends FragmentActivity {
 			}
 		});
 
-		buttonsEnabled(false);
+//		buttonsEnabled(false);
 	}
+	
+	public View onCreateView(
+	        LayoutInflater inflater,
+	        ViewGroup container,
+	        Bundle savedInstanceState) {
+	    View view = inflater.inflate(R.layout.splash, container, false);
 
+//	    LoginButton loginButton = (LoginButton) view.findViewById(R.id.login_button);
+	    loginButton.setReadPermissions("user_friends");
+	    // If using in a fragment
+	    loginButton.setFragment(this);    
+	    // Other app specific specialization
+
+	    // Callback registration
+	    loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+	        @Override
+	        public void onSuccess(LoginResult loginResult) {
+	            // App code
+	        }
+
+	        @Override
+	        public void onCancel() {
+	            // App code
+	        }
+
+	        @Override
+	        public void onError(FacebookException exception) {
+	            // App code
+	        }
+	    });    
+	}
+	
+/*
 	private Session.StatusCallback statusCallback = new Session.StatusCallback() {
 		@Override
 		public void call(Session session, SessionState state,
@@ -181,4 +278,5 @@ public class MainActivity extends FragmentActivity {
 		super.onSaveInstanceState(savedState);
 		uiHelper.onSaveInstanceState(savedState);
 	}
+*/	
 }
