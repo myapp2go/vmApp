fc932530d98e305249d12218e4fe0b5bed5613b1
myapp2go <tapaulchen@gmail.com>
2015-12-04 14:00:49 -0800

    init

---
+++ appGo/src/com/app2go/appgo/Constants.java
@@ -0,0 +1,96 @@
+package com.app2go.appgo;
+
+public final class Constants {
+
+	private static final String Activity_Recognition = "https://github.com/myapp2go/vmApp.git";
+
+	public static final int MAIL_PER_PAGE = 1;
+	
+	public static final String EMPTY_STRING = "";
+	public static final String CONTACT_MARKER = "__M_A_R_K_E_R__";
+
+	public static String COMMAND_GREETING = "Please speak read, write or setting";
+
+	public static String MAIL_BODY_NOT_SUPPORT = "This mail message in not plain text";
+	public static String MAIL_BODY_IS_HTML = "This mail message type is html";
+	public static String MAIL_BODY_IS_MULTIPART = "This mail message is multipart";
+	public static String MAIL_BODY_HAVE_ATTACHMENT = "This mail message have attachmednt";
+	
+	public static final String COMMAND_SKIP_GREETING = "skip mail";
+	
+	public static final String COMMAND_TO_GREETING = "Who you want to send";
+	public static final String COMMAND_SUBJECT_GREETING = "Please speak subject";
+	public static final String COMMAND_BODY_GREETING = "Please speak message";
+	public static final String COMMAND_DONE_GREETING = "Please speak send or add or save or stop";
+	public static final String COMMAND_ECHO_HEADER_GREETING = "You say ";
+	public static final String COMMAND_ECHO_FOOTER_GREETING = " yes or no ";
+	public static final String COMMAND_ADVERTISE_SUBJECT = "Timebyte IT outsourcing ";
+	public static final String COMMAND_ADVERTISE_BODY = "Delivers total solutions that satisfy both quality, efficiency and cost reduction requirements.";
+
+	// READ
+	public static String COMMAND_READ_GREETING = "Please speak 1 for reading subject, 2 for reading subject and message";
+	public static String COMMAND_READ_SUBJECT = "voice command 1 for continue, 2 for stop";
+	public static String COMMAND_READ_SUBJECT_BODY = "voice command 1 for next mail, 2 for mail detail, 3 for skip detail, 4 for stop";	
+//	public static String COMMAND_READ_RETRIEVE = "Retrieve mail, please wait";	
+	
+	public static final String COMMAND_INIT = "init";
+	public static final String COMMAND_NONE = "none";
+	public static final String COMMAND_QUIT = "quit";
+	public static final String COMMAND_STOP = "stop";
+
+	
+	public static final String COMMAND_READ = "read";
+	public static final String SUBCOMMAND_RETRIEVE = "retrieve";
+//	public static final String SUBCOMMAND_MORE_SKIP = "moreSkip";
+	public static final String SUBCOMMAND_SKIP = "skip";
+	public static final String SUBCOMMAND_DETAIL = "detail";
+	public static final String SUBCOMMAND_REPEAT = "repeat";
+	public static final String SBCOMMAND_UP = "up";
+	public static final String SBCOMMAND_DOWN = "down";
+	public static final String SBCOMMAND_NEXT = "next";
+	public static final String READ_OPTION_SUBJECT_ONLY = "subjectOnly";
+	public static final String READ_OPTION_SUBJECT_BODY = "subjectBody";
+
+	public static String COMMAND_SEARCH_GREETING = "search";
+
+	public static String SETTING_ACCOUNT_NOTICE = "You did not set up your account yet";
+	public static String SETTING_CONTACT_NOTICE = "You did not set up your contact list yet";
+	public static String SETTING_ACCOUNT_ERROR = "Please verify your account and password.";
+	public static String NETWORK_ERROR = "Please verify your network connection.";
+	public static String MAIL_SERVER_NOT_SUPPORT = "This mail server is not support yet.";
+
+	public static final String COMMAND_WRITE = "write";
+	public static final String SUBCOMMAND_TO = "to";
+	public static final String SUBCOMMAND_VERIFY_TO = "verifyTo";
+	public static final String SUBCOMMAND_SUBJECFT = "subject";
+	public static final String SUBCOMMAND_BODY = "body";
+	public static final String SUBCOMMAND_DONE = "done";
+	public static final String SUBCOMMAND_ADD = "add";
+	public static final String SUBCOMMAND_SAVE = "save";
+	public static final String SUBCOMMAND_SEND = "send";
+	public static final String SUBCOMMAND_MESSAGE = "message";
+	
+	public static final String COMMAND_SETTING = "setting";
+
+	public static final String COMMAND_SEARCH = "search";
+	public static final String COMMAND_OFFLINE = "offLine";
+	
+	public static final String COMMAND_COMMAND_RECORD = "commandRecord";
+	public static final String COMMAND_COMMAND1_GREETING = "please speak 1";
+	public static final String COMMAND_COMMAND2_GREETING = "please speak 2";
+	public static final String COMMAND_COMMAND3_GREETING = "please speak 3";
+	public static final String COMMAND_COMMAND4_GREETING = "please speak 4";
+	
+	public static final String ANSWER_CLEAN = "-1";
+	public static final String ANSWER_SAVE = "0";
+	public static final String ANSWER_CONTINUE = "1";
+	public static final String ANSWER_DETAIL = "2";
+	public static final String ANSWER_SKIP = "3";
+	public static final String ANSWER_STOP = "4";
+	public static final String ANSWER_REPEAT = "5";
+	public static final String ANSWER_NOT_VALID = "Please speak 1 to 4";
+	
+	public static final String ANSWER_YES = "yes";
+	public static final String ANSWER_NO = "no";
+	
+}
