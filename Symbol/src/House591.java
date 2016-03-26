a73eb0ab27450343e96f20d8372e454624fb58ad
myapp2go <tapaulchen@gmail.com>
2016-03-26 09:47:20 -0700

    591

--- Symbol/src/House591.java
+++ Symbol/src/House591.java
@@ -1,37 +1,164 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+import java.util.Calendar;
 
 import org.jsoup.Jsoup;
 
 public class House591 extends SinYi {
 
-	private static String file591 = "C:\\Users\\mspau\\git\\vmApp\\Symbol\\src\\House591House.txt";
+	private static String house591File = "C:\\Users\\mspau\\git\\vmApp\\Symbol\\src\\house591House.txt";
+	private static int house591Count = 1;
+	private static int lineCount = house591Count*20*4;
+	protected static String[][] house591Data = new String[fieldCount][lineCount];
 
 	public static void main(String[] args) {		
 		House591 house = new House591();
-		house.get591House();
+		
+//		house.readHouse(house591File, house591Data);
+//		house.getHouse591(house591File, house591Data);
 		
 		house.readHouse(sinyiFile, sinyiData);
-
 		house.getSinYi(sinyiFile, sinyiData);
 	}
 	
-	protected void get591House() {
+	void getHouse591(String name, String[][] data) {
+		try {
+			Writer w = new OutputStreamWriter(new FileOutputStream(name), "UTF-8");
+
+			for (int i = 1; i <= house591Count; i++) {
+				procHouse591(w, i);
+			}
+			
+			procDelete(w, data, lineCount);
+			
+			w.close();
+		} catch (UnsupportedEncodingException | FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}	
+	}
+	
+	protected void procHouse591(Writer w, int fileCount) {
 		String doc = "";
 		String url = "https://m.591.com.tw/mobile-list.html?version=1&type=sale&regionid=3&sectionidStr=37&kind=9&price=4";
 			
 		try {
 			doc = Jsoup.connect(url).get().html();
-			get591(doc, 1);			
+			
+			int nameInd = doc.indexOf("data-house-id");
+			int count = 0;
+			while (nameInd > 0 && count < 50) {
+				count++;
+				parseHouse591(doc, w, nameInd);
+
+//				boxInd = doc.indexOf("item_titlebox", nameInd+20);
+				nameInd = doc.indexOf("data-house-id", nameInd+20);
+			}
+//			get591(doc, w);			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	private void get591(String doc, int count) {
-		int nameInd = doc.indexOf("data-house-id");
+	private void parseHouse591(String doc, Writer w, int ind) {
+//		int nameInd = doc.indexOf("data-house-id");
 		
-		System.out.println(doc.substring(nameInd, nameInd+100));
+		try {
+			int start = ind+15;
+			int end = doc.indexOf("\"", start);
+			String hid = doc.substring(start, end);	
+			String floor = "";
+			String year = "";
+			String[] info = new String[2];
+			boolean skip = getMoreInfo(hid, info);
+			if (!skip) {
+				w.append("\n" + "N" + '\t');
+				
+				w.append(hid + '\t');
+
+				// floor
+				w.append(info[0] + '\t');
+
+				// price
+				start = doc.indexOf("n1\"", end) + 10;
+				end = doc.indexOf("<", start);
+				System.out.println(doc.substring(start, end));
+				w.append(doc.substring(start, end) + '\t');
+
+				w.append(doc.substring(start, end) + '\t');
+
+				// year
+				w.append(info[1] + '\t');
+				
+				// room
+				start = doc.indexOf("<span", end) + 6;
+				end = doc.indexOf(" ", start);
+				w.append(doc.substring(start, end) + '\t');
+
+				// size
+				start = end + 1;
+				end = doc.indexOf("<", start);
+				w.append(doc.substring(start, end) + '\t');
+
+				w.append(doc.substring(start, end) + '\t');
+
+				w.append(doc.substring(start, end) + '\t');
+
+				// address
+				start = doc.indexOf("n2\"", end);
+				start = doc.indexOf("span>", start) + 5;
+				end = doc.indexOf("<", start);
+				w.append(doc.substring(start, end) + '\t');
+
+				// title
+				start = doc.indexOf("n3\"", end) + 4;
+				end = doc.indexOf("<", start);
+				System.out.println(doc.substring(start, end));
+				w.append(doc.substring(start, end) + '\t');
+				
+				// date
+				w.append(Calendar.getInstance().getTime().toString() + '\t');
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}				
+	}
+
+	private boolean getMoreInfo(String hid, String[] info) {
+		String doc = "";
+		String url = "https://m.591.com.tw/mobile-detail.html?type=sale&regionid=3&sectionidStr=37&price=4&room=&area=&kind=9&shape=&firstRow=0&houseId="+hid;
+			
+		try {
+			doc = Jsoup.connect(url).get().html();
+			
+			int nameInd = doc.indexOf("price_num");
+			if (nameInd > 0) {
+				int start = nameInd+170;
+				start = doc.indexOf(">", start)+1;
+				int end = doc.indexOf("<", start);
+				info[0] = doc.substring(start, end);
+				
+				// skip 2 <b> tab
+				start = doc.indexOf("b>", end)+3;
+				start = doc.indexOf("b>", start)+2;
+				end = doc.indexOf("<", start);
+				info[1] = doc.substring(start, end);
+		
+			}
+//			get591(doc, w);			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
 	}
 
 }
--- Symbol/src/SinYi.java
+++ Symbol/src/SinYi.java
@@ -77,60 +77,103 @@ public class SinYi extends PCHouse {
 		
 		try {
 			String id = doc.substring(end+2, end+9);
-			mode = checkID(id, sinyiData, lineCount);
-			if (mode != null) {
-				w.append("\n"+mode+"\t");
-			} else {
-				w.append("\nN\t");
-			}
 			
-			w.append(id + '\t');
-			w.append(doc.substring(start, end) + '\t');
+			mode = checkID(id, sinyiData, lineCount);
+			String strMode = "";
+			if (mode != null) {
+				strMode = "\n"+mode+"\t";
+			} else {
+				strMode = "\nN\t";
+			}
+						
+			// title
+			String title = doc.substring(start, end);
 
+			// address
 			start = doc.indexOf("detail_line1", end);
 			start = doc.indexOf("html-tag", start);
 			start = doc.indexOf("span>", start);
 			end = doc.indexOf("<", start);
-			w.append(doc.substring(start+5, end) + '\t');
+			String address = doc.substring(start+5, end);
 
+			// size 1
 			start = doc.indexOf("detail_line2", end);
 			start = doc.indexOf("num<", start)+22;
 			end = doc.indexOf("<", start);
-			w.append(doc.substring(start, end) + '\t');
+			String size1 = doc.substring(start, end);
 			
+			// size 2
 			start = doc.indexOf("num<", end)+22;
 			end = doc.indexOf("<", start);
-			w.append(doc.substring(start, end) + '\t');
+			String size2 = doc.substring(start, end);
 
 			// year old
 			start = doc.indexOf("num<", end)+22;
 			end = doc.indexOf("<", start);
-			w.append(doc.substring(start, end) + '\t');
+			String year = doc.substring(start, end);
 
 			// floor
 			start = doc.indexOf("num<", end)+22;
 			end = doc.indexOf("<", start);
-			w.append("#" + doc.substring(start, end) + '\t');
+			String floorNum = doc.substring(start, end);
+			boolean isSkip = checkyear(floorNum);
+			if (!isSkip) {
+				// mode
+				w.append(strMode);
+				
+				// id
+				w.append(id + '\t');
 
-			start = doc.indexOf("price_old", end);
-			int comp = doc.indexOf("price_new", end);
-			if (start > 0 && start < comp) {
-				start += 28;
+				// floor
+				w.append("#" + floorNum + '\t');
+
+				// room
+				start = doc.indexOf("num<", end)+22;
+				end = doc.indexOf("<", start);
+				String room = doc.substring(start, end);
+				
+				// price_old
+				start = doc.indexOf("price_old", end);
+				int comp = doc.indexOf("price_new", end);
+				if (start > 0 && start < comp) {
+					start += 28;
+					end = doc.indexOf("<", start);
+					w.append(doc.substring(start, end) + '\t');
+				} else {
+					w.append("XXX" + '\t');
+				}
+
+				// price_new
+				start = doc.indexOf("price_new", end) + 28;
+				start = doc.indexOf("num", start) + 22;
 				end = doc.indexOf("<", start);
 				w.append(doc.substring(start, end) + '\t');
-			} else {
-				w.append("XXX" + '\t');
+
+				w.append(year + '\t');
+
+				w.append(room + '\t');
+				
+				w.append(size1 + '\t');
+
+				w.append(size2 + '\t');
+
+				w.append(size2 + '\t');
+
+				w.append(address + '\t');
+
+				w.append(title + '\t');
+				
+				// date
+				w.append(Calendar.getInstance().getTime().toString() + '\t');
 			}
-			
-			start = doc.indexOf("price_new", end) + 28;
-			start = doc.indexOf("num", start) + 22;
-			end = doc.indexOf("<", start);
-			w.append(doc.substring(start, end) + '\t');
-			
-			w.append(Calendar.getInstance().getTime().toString() + '\t');
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private boolean checkyear(String floorNum) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
