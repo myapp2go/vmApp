0865a409167068f7ef6b3691bf8939bee1526f72
myapp2go <tapaulchen@gmail.com>
2017-04-26 20:35:51 -0700

    new

--- Symbol/src/house/YungChing.java
+++ Symbol/src/house/YungChing.java
@@ -18,98 +18,28 @@ public class YungChing extends PCHouse {
 	private static int pageSize = 30;
 	
 	public static void main(String[] args) {
-		System.out.println("YungChing Start");
 		YungChing house = new YungChing();
-	
-		for (int i = 0; i < houseList.length; i++) {
-			setCityZip(houseList[i]);
-			System.out.println("Yahoo " + houseList[i]);
-
-			String compFile = "C:\\Users\\" + GITLOC + "\\git\\vmApp\\Symbol\\src\\data\\yungching_"+getCityZip()+"_House.txt";
-			String[][] compData = new String[constFieldCount][pageCount*pageSize*constExtraCount];
-
-//			house.readHouse(compFile, compData);
-			house.getSinYi(compFile, compData);
-		}
-		System.out.println("YungChing Done");
-	}
-	
-	void getSinYi(String name, String[][] data) {
-		try {
-			shareLinkCount = 2;
-			
-			Writer w = new OutputStreamWriter(new FileOutputStream(name), "UTF-8");
-
-			boolean found = true;
-			for (int i = 1; found && i <= pageCount; i++) {
-				found = procYungChing(w, i, data);
-				System.out.println("Page " + i);
-			}
-			postProc(w, data, constDataCount);
-			
-			w.close();
-		} catch (UnsupportedEncodingException | FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}	
-	}
-	
-	private boolean procYungChing(Writer w, int fileCount, String[][] data) {
-		boolean found = true;
-		StringBuffer doc = new StringBuffer();
-        File f = new File("C:\\logs\\house\\" + getCityZip() + "+_yc_" + fileCount + ".html");
+		String srcFileName = "yungching_";
 		
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(
-			        new FileInputStream("C:\\logs\\house\\" + getCityZip() + "_yc_" + fileCount + ".html"), "UTF-8"));
-			
-			String sCurrentLine;
-			while ((sCurrentLine = br.readLine()) != null) {
-				doc.append(sCurrentLine);
-			}
-		} catch (FileNotFoundException e1) {
-			found = false;
-			return found;
-		} catch (IOException e1) {
-			found = false;
-			return found;
-		}
-
+		startHouse(house, srcFileName);
+	}
+	
+	protected int getHouseData(Writer w, StringBuffer doc, String[][] data, String yearArray[][][], int yearIndex) {
 		int nameInd = doc.indexOf("m-list-item");
-		int boxInd = -99; doc.indexOf("item_titlebox");
-		String yearArray[][][] = new String[2][2][100];
-		yearArray[0][0][0] = "99";
-		int yearIndex = 1;
 		while (nameInd > 0) {
-//			if (nameInd != boxInd) {
-				parseYungChing(doc, w, nameInd, data, yearArray, yearIndex);
-				yearIndex++;
-//			}
-				
-//			boxInd = doc.indexOf("item_titlebox", nameInd+20);
+			parseYungChing(doc, w, nameInd, data, yearArray, yearIndex);
+			yearIndex++;
 			nameInd = doc.indexOf("m-list-item", nameInd+20);
 		}
 		
-		try {
-			for (int i = 0; i < yearIndex; i++) {			
-				w.append(yearArray[1][1][i]);
-				w.append("\r\n");
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return found;
+		return yearIndex;
 	}
 
 	private void parseYungChing(StringBuffer doc, Writer w, int nameInd, String[][] data, String yearArray[][][], int yearIndex) {
 		int start = doc.indexOf("title", nameInd) + 7;
-		int end = doc.indexOf("\" ", start);		
+		int end = doc.indexOf("\"", start);		
 		
-
-			String id = doc.substring(end+2, end+9);
+//			String id = doc.substring(end+2, end+9);
 
 			String[] info = new String[constInfoSize];
 			boolean skip = false; // checkID(id, data, constDataCount, info);
@@ -122,6 +52,8 @@ public class YungChing extends PCHouse {
 				start = doc.indexOf("item-description", end);
 				start = doc.indexOf(">", start);
 				String itemId = doc.substring(start + 1, start + 8);
+				
+				String retMark = checkNewID(itemId, data, constDataCount, info);
 				
 				// year
 				start = doc.indexOf("item-info-detail", end);
@@ -156,8 +88,10 @@ public class YungChing extends PCHouse {
 				start = doc.indexOf("price-num", start + 12);
 				end = doc.indexOf("</span", start+5);
 				String price = doc.substring(start + 11, end);	
+				price = price.replace(",", "");
 				
-				result.append('\t');
+				result.append(retMark + '\t');
+				result.append(itemId + '\t');
 				result.append('\t');
 				result.append('\t');
 				result.append('\t');
@@ -182,67 +116,9 @@ public class YungChing extends PCHouse {
 				result.append('\t');
 				result.append('\t');
 				result.append('\t');
-				result.append(itemId + '\t');
 				
 				sortYear(yearArray, year, price, result.toString(), yearIndex);
-				
-//				sortUear(yearArray);
-//				w.append(result);
-//				w.append("\r\n");
-/*
-				// date
-				if (info[5] != null) {
-					w.append(info[5] + '\t');					
-				} else {
-					w.append(Calendar.getInstance().getTime().toString() + '\t');
-				}
-				
-				String houseUrl = "http://buy.sinyi.com.tw/house/" + id.substring(1) + ".html";
-				w.append(houseUrl + '\t');
-				
-				w.append("=HYPERLINK(N" + (shareLinkCount++) +")" + '\t');
-				
-				if (changePrice.length() > 0) {
-					w.append(info[4] + "|" + info[3] + "|" + Calendar.getInstance().getTime().toString() + '\t');
-				}
-				*/
 			}
-
-	}
-/*
-	private void sortYear(String[][][] yearArray, String year, String price,
-			String result, int yearIndex) {
-		Float yearVal = Float.valueOf(year);
-		
-		String old = yearArray[0][0][0];
-		Float oldVal = Float.valueOf(old);
-		int index = 0;
-		while (yearVal > oldVal && oldVal != 99.0) {
-			index++;
-			old = yearArray[0][0][index];
-			oldVal = Float.valueOf(old);
-		}
-		
-		for (int i = yearIndex; i > index; i--) {
-			yearArray[0][0][i] = yearArray[0][0][i-1];
-			yearArray[0][1][i] = yearArray[0][1][i-1];
-			yearArray[1][1][i] = yearArray[1][1][i-1];
-		}
-		
-		yearArray[0][0][index] = year;
-		yearArray[0][1][index] = price;
-		yearArray[1][1][index] = result;
-	}
-*/
-	private float toYearNum(String year) {
-		// TODO Auto-generated method stub
-		java.lang.Character.isDigit(year.charAt(0));
-		return 0;
-	}
-
-	private boolean checkyear(String floorNum) {
-		// TODO Auto-generated method stub
-		return false;
 	}
 
 }
