ec1a2df92ba53f452e795cdaa80fb958ab5f0f76
myapp2go <tapaulchen@gmail.com>
2017-04-27 04:05:21 -0700

    new

--- Symbol/src/house/HouseFun.java
+++ Symbol/src/house/HouseFun.java
@@ -18,90 +18,23 @@ public class HouseFun extends PCHouse {
 	private static int pageSize = 30;
 	
 	public static void main(String[] args) {
-		System.out.println("HouseFun Start");
 		HouseFun house = new HouseFun();
 	
-		for (int i = 0; i < houseList.length; i++) {
-			setCityZip(houseList[i]);
-			System.out.println("Yahoo " + houseList[i]);
-
-			String compFile = "C:\\Users\\" + GITLOC + "\\git\\vmApp\\Symbol\\src\\data\\HouseFun_"+getCityZip()+"_House.txt";
-			String[][] compData = new String[constFieldCount][pageCount*pageSize*constExtraCount];
-
-//			house.readHouse(compFile, compData);
-			house.getCTWarm(compFile, compData);
-		}
-		System.out.println("HouseFun Done");
-	}
-	
-	void getCTWarm(String name, String[][] data) {
-		try {
-			shareLinkCount = 2;
-			
-			Writer w = new OutputStreamWriter(new FileOutputStream(name), "UTF-8");
-
-			boolean found = true;
-			for (int i = 1; found && i <= pageCount; i++) {
-				found = procHouseFun(w, i, data);
-				System.out.println("Page " + i);
-			}
-			postProc(w, data, constDataCount);
-			
-			w.close();
-		} catch (UnsupportedEncodingException | FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}	
-	}
-	
-	private boolean procHouseFun(Writer w, int fileCount, String[][] data) {
-		boolean found = true;
-		StringBuffer doc = new StringBuffer();
-        File f = new File("C:\\logs\\house\\" + getCityZip() + "+_yc_" + fileCount + ".html");
+		String srcFileName = "housefun_";
 		
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(
-			        new FileInputStream("C:\\logs\\house\\" + getCityZip() + "_housefun_" + fileCount + ".html"), "UTF-8"));
-			
-			String sCurrentLine;
-			while ((sCurrentLine = br.readLine()) != null) {
-				doc.append(sCurrentLine);
-			}
-		} catch (FileNotFoundException e1) {
-			found = false;
-			return found;
-		} catch (IOException e1) {
-			found = false;
-			return found;
-		}
-
+		startHouse(house, srcFileName);
+	}
+	
+	protected int getHouseData(Writer w, StringBuffer doc, String[][] data, String yearArray[][][], int yearIndex) {
 		int nameInd = doc.indexOf("m-list-obj");
-//		int boxInd = doc.indexOf("objlist__ItemWrap end");
-		String yearArray[][][] = new String[2][2][100];
-		yearArray[0][0][0] = "99";
-		int yearIndex = 1;
-		while (nameInd > 0) {
-//			if (boxInd - nameInd > 500) {
-				parseHouseFun(doc, w, nameInd, data, yearArray, yearIndex);
-				yearIndex++;
-//			}
-				
-//			boxInd = doc.indexOf("item_titlebox", nameInd+20);
+		int indEnd = doc.indexOf("m-list-obj ");
+		while (nameInd > 0 && nameInd != indEnd) {
+			parseHouseFun(doc, w, nameInd, data, yearArray, yearIndex);
+			yearIndex++;
 			nameInd = doc.indexOf("m-list-obj", nameInd+300);
 		}
 		
-		try {
-			for (int i = 0; i < yearIndex; i++) {			
-				w.append(yearArray[1][1][i]);
-				w.append("\r\n");
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return found;
+		return yearIndex;
 	}
 
 	private void parseHouseFun(StringBuffer doc, Writer w, int nameInd, String[][] data, String yearArray[][][], int yearIndex) {
@@ -162,11 +95,8 @@ public class HouseFun extends PCHouse {
 				start = doc.indexOf("number", end+10);
 				end = doc.indexOf("</", start+5);
 				String price = doc.substring(start + 8, end);	
-/*
-				start = doc.indexOf("number", end+10);
-				end = doc.indexOf("</", start+5);
-				String price = doc.substring(start + 7, end);	
-*/				
+				price = price.replace(",", "");
+							
 				result.append('\t');
 				result.append('\t');
 				result.append('\t');
@@ -204,64 +134,8 @@ public class HouseFun extends PCHouse {
 				result.append(priceOld + '\t');
 				
 				sortYear(yearArray, year, price, result.toString(), yearIndex);
-				
-//				sortUear(yearArray);
-//				w.append(result);
-//				w.append("\r\n");
-/*
-				// date
-				if (info[5] != null) {
-					w.append(info[5] + '\t');					
-				} else {
-					w.append(Calendar.getInstance().getTime().toString() + '\t');
-				}
-				
-				String houseUrl = "http://buy.sinyi.com.tw/house/" + id.substring(1) + ".html";
-				w.append(houseUrl + '\t');
-				
-				w.append("=HYPERLINK(N" + (shareLinkCount++) +")" + '\t');
-				
-				if (changePrice.length() > 0) {
-					w.append(info[4] + "|" + info[3] + "|" + Calendar.getInstance().getTime().toString() + '\t');
-				}
-				*/
 			}
 
-	}
-/*
-	private void sortYear(String[][][] yearArray, String year, String price,
-			String result, int yearIndex) {
-		Float yearVal = Float.valueOf(year);
-		
-		String old = yearArray[0][0][0];
-		Float oldVal = Float.valueOf(old);
-		int index = 0;
-		while (yearVal > oldVal && oldVal != 99.0) {
-			index++;
-			old = yearArray[0][0][index];
-			oldVal = Float.valueOf(old);
-		}
-		
-		for (int i = yearIndex; i > index; i--) {
-			yearArray[0][0][i] = yearArray[0][0][i-1];
-			yearArray[0][1][i] = yearArray[0][1][i-1];
-			yearArray[1][1][i] = yearArray[1][1][i-1];
-		}
-		
-		yearArray[0][0][index] = year;
-		yearArray[0][1][index] = price;
-		yearArray[1][1][index] = result;
-	}
-*/
-	private float toYearNum(String year) {
-		// TODO Auto-generated method stub
-		java.lang.Character.isDigit(year.charAt(0));
-		return 0;
-	}
-
-	private boolean checkyear(String floorNum) {
-		// TODO Auto-generated method stub
-		return false;
 	}
 
 }
